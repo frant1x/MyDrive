6a46334325ecc7d3f5c1197b492931323c0dc2fc
Nazar Pyskor <pyskornazik@gmail.com>
2026-08-05 14:03:59 +0300

    Add FileViewSet and endpoints. Complete v1 (direct file upload into PostgreSQL)

--- backend/files/views.py
+++ backend/files/views.py
@@ -1,1 +1,21 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.parsers import FormParser, MultiPartParser
+from .serializers import FileSerializer, FileUpdateSerializer
+from .models import File
 
+
+class FileViewSet(ModelViewSet):
+    parser_classes = [MultiPartParser, FormParser]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        return File.objects.filter(user=user).defer("file")
+
+    def get_serializer_class(self):
+        if self.action in ["update", "partial_update"]:
+            return FileUpdateSerializer
+        return FileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
